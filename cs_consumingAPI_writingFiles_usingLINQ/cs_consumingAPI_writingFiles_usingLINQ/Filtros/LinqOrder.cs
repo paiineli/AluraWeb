794adb910c880333b0d8c1f136ca8cf16e3c312e
Lucas Paineli <painelilucas@gmail.com>
2024-10-21 18:07:49 -0300

    create new folder

---
+++ cs_consumingAPI_writingFiles_usingLINQ/cs_consumingAPI_writingFiles_usingLINQ/Filtros/LinqOrder.cs
@@ -0,0 +1,16 @@
+﻿using cs_consumingAPI_writingFiles_usingLINQ.Modelos;
+
+namespace cs_consumingAPI_writingFiles_usingLINQ.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+    {
+        var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artistas ordenados");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
---
+++ cs_consumingAPI_writingFiles_usingLINQ/cs_consumingAPI_writingFiles_usingLINQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using cs_consumingAPI_writingFiles_usingLINQ.Modelos;
+using cs_consumingAPI_writingFiles_usingLINQ.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+
+        //musicas[1].ExibirDetalhesDaMusica();
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        ///Lin/Filter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+        //var musicasPreferidasPaineli = new MusicasPreferidas("Paineli");
+        //musicasPreferidasPaineli.AdicionarMusicasFavoritas(musicas[7]);
+        //musicasPreferidasPaineli.AdicionarMusicasFavoritas(musicas[14]);
+        //musicasPreferidasPaineli.AdicionarMusicasFavoritas(musicas[21]);
+        //musicasPreferidasPaineli.AdicionarMusicasFavoritas(musicas[28]);
+        //musicasPreferidasPaineli.AdicionarMusicasFavoritas(musicas[35]);
+
+        //musicasPreferidasPaineli.ExibirMusicasFavoritas();
+
+        var musicasPreferidasGimenes = new MusicasPreferidas("Gimenes");
+        musicasPreferidasGimenes.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasGimenes.AdicionarMusicasFavoritas(musicas[12]);
+        musicasPreferidasGimenes.AdicionarMusicasFavoritas(musicas[18]);
+        musicasPreferidasGimenes.AdicionarMusicasFavoritas(musicas[24]);
+        musicasPreferidasGimenes.AdicionarMusicasFavoritas(musicas[30]);
+
+        musicasPreferidasGimenes.ExibirMusicasFavoritas();
+        musicasPreferidasGimenes.GerarArquivoJson();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
